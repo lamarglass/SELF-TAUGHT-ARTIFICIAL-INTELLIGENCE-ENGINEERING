eb353afa3d416d303176e2885c4f84ca953cc132
lamarglass <lamarvantaglass@gmail.com>
2025-09-11 17:11:28 +0300

    S.T-A.I

---
+++ Foundation (AI Science + Engineering Basics)/Python-Programming:Focus-on-AI/from: mosh/largest number in a list.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 7, 10, 3, 2, 1]
+
+max = numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+print(max)        
---
+++ Foundation (AI Science + Engineering Basics)/Python-Programming:Focus-on-AI/from: mosh/list.py
@@ -0,0 +1,5 @@
+names = ["Rita", "maria", "pauline", "Dorcas"]
+
+names[0] = "Lamar"
+print(names[3])
+print(names)
